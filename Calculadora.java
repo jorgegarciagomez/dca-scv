f028bbabef616838a68be9ef0359b6b3fa81e84e
Jorge Garcia Gomez <jgg107@alu.ua.es>
2016-12-24 00:14:41 +0100

    tercera suma

--- Calculadora.java
+++ Calculadora.java
@@ -3,6 +3,7 @@ public class Calculadora {
   public static void main(String[] args) {
 	System.out.println("Resultado de la primera suma: " + sumar(1,3));
 	System.out.println("Resultado de la segunda suma: " + sumar(1,3));
+	System.out.println("Resultado de la tercera suma: " + sumar(1,n));
   }
   public static int sumar(int a, int b) {
     return a + b;
