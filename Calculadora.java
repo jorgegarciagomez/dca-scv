a15d6a7b11e88548bb044229ff58b1ebdac5987a
Jorge Garcia Gomez <jgg107@alu.ua.es>
2016-12-24 00:16:08 +0100

    quinta summa

--- Calculadora.java
+++ Calculadora.java
@@ -5,6 +5,7 @@ public class Calculadora {
 	System.out.println("Resultado de la segunda suma: " + sumar(1,3));
 	System.out.println("Resultado de la tercera suma: " + sumar(1,n));
 	System.out.println("Resultado de la cuarta suma: " + sumar(1,n));
+	System.out.println("Resultado de la quinta suma: " + sumar(1,n));
   }
   public static int sumar(int a, int b) {
     return a + b;
