e819d6db03e34b2db02b7bd0464af259e30a4354
Jorge Garcia Gomez <jgg107@alu.ua.es>
2016-12-24 00:14:14 +0100

    seguna suma

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,8 @@
 public class Calculadora {
   public Calculadora() {}
   public static void main(String[] args) {
-    System.out.println("Resultado de la primera suma: " + sumar(1,3));
+	System.out.println("Resultado de la primera suma: " + sumar(1,3));
+	System.out.println("Resultado de la segunda suma: " + sumar(1,3));
   }
   public static int sumar(int a, int b) {
     return a + b;
